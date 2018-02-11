434b919db874ca32325b59c8d7b66b01081338ad
Jodi <joday_@hotmail.com>
2018-02-11 13:11:33 +1100

    ssl v2

--- wp-config.php
+++ wp-config.php
@@ -66,12 +66,10 @@ $table_prefix  = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 /** SSL */  
-define('FORCE_SSL_ADMIN', true);  
-// in some setups HTTP_X_FORWARDED_PROTO might contain  
-// a comma-separated list e.g. http,https  
-// so check for https existence  
-if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)  
-    $_SERVER['HTTPS']='on';
+define('FORCE_SSL_ADMIN', true);
+define('FORCE_SSL_LOGIN', true);
+if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+$_SERVER['HTTPS']='on';
 
 
 /* That's all, stop editing! Happy blogging. */
